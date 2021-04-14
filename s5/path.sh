a535159efa6346e7904b1afdef7f2ecc46c2a2f0
Camille Goudeseune <cog@illinois.edu>
2021-04-14 16:07:59 -0500

    source env.sh

--- s5/path.sh
+++ s5/path.sh
@@ -1,16 +1,16 @@
-export KALDI_ROOT=`pwd`/../../..
+export KALDI_ROOT=$PWD/../../..
 export PATH=$PWD/utils/:$KALDI_ROOT/src/bin:$KALDI_ROOT/tools/openfst/bin:$KALDI_ROOT/src/fstbin/:$KALDI_ROOT/src/gmmbin/:$KALDI_ROOT/src/featbin/:$KALDI_ROOT/src/lm/:$KALDI_ROOT/src/sgmmbin/:$KALDI_ROOT/src/sgmm2bin/:$KALDI_ROOT/src/fgmmbin/:$KALDI_ROOT/src/latbin/:$KALDI_ROOT/src/onlinebin/:$KALDI_ROOT/src/lmbin/:$PWD:$PATH
 
-# VoxForge data will be stored in:
-export DATA_ROOT="/tmp/voxforge"    # e.g. something like /media/secondary/voxforge
-
+export DATA_ROOT="/tmp/voxforge" # e.g., /media/secondary/voxforge
 if [ -z $DATA_ROOT ]; then
-  echo "In path.sh, point the \"DATA_ROOT\" variable to the directory that will host VoxForge's data"
+  echo "In s5/path.sh, point DATA_ROOT to the directory that will hold VoxForge's data."
   exit 1
 fi
 
 # Make sure that MITLM shared libs are found by the dynamic linker/loader
-export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:$(pwd)/tools/mitlm-svn/lib
+export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:$PWD/tools/mitlm-svn/lib
+
+source $KALDI_ROOT/tools/env.sh
 
 # Needed for "correct" sorting
 export LC_ALL=C
